43a02b37775227fbc75f500fcf77ccea73e64dcc
Terbuh <hubertt.popowicz@gmail.com>
2021-10-16 17:01:34 +0200

    feat(update): change done with mentor

--- app/index.ts
+++ app/index.ts
@@ -29,47 +29,9 @@ async function getDataCompanies<CompanyType>(url: string): Promise<CompanyType> 
 const usersDataX = getDataUser<UserType[]>(urlUsers);
 const companiesDataX = getDataCompanies<CompanyType[]>(urlCompanies);
 
-// async function getData<T>(url: string): Promise<T> {
-//     let res = await fetch(url);
-//     return await res.json();
-// }
-//
-// const usersData = getData<UserType>(urlUsers);
-// const companiesData = getData<CompanyType>(urlCompanies);
-//
-// console.log('userDataX',usersData);
-// console.log('companiesDataX',companiesData);
-
-
 const renderCompaniesAndItsUsers = async () => {
-    let users = await usersData;
-    let companies = await companiesData();
-
-
-    let template = '';
-    companies.forEach(company => {
-        template +=`
-                <table class="company">
-                <th>Name: ${company.name}</th>
-                 </table>`
-
-        users.forEach(user =>  {
-            if ( company.uri === user.uris.company ) {
-                template += `
-                  <table class="user">
-                  <td>Name: ${user.name},</td>
-                  <td>email: ${user.email}</td>
-                  </table>`
-            }
-        })
-    })
-    container3.innerHTML = template;
-}
-renderCompaniesAndItsUsers();
-
-const renderCompaniesAndItsUsers = async () => {
-    let users = await usersData;
-    let companies = await companiesData;
+    let users = await usersDataX;
+    let companies = await companiesDataX;
 
     let template = '';
     companies.forEach(company => {
--- webpack.config.js
+++ webpack.config.js
@@ -2,44 +2,24 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: {
-    bundle: './app/index.js'
-  },
-  output: {
-    filename: '[name].[hash].js',
-    path: path.resolve(__dirname, 'dist')
-  },
+  entry: './app/index.ts',
   module: {
     rules: [
       {
-        test: /\.js$/, loader: 'eslint-loader', exclude: /node_modules/
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
       },
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(ttf|eot|woff|woff2)$/,
-        loader: 'url-loader?limit=10000&minetype=application/font-woff'
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 5000
-            }
-          }
-        ]
-      }
     ],
   },
-  devtool: 'eval-source-map',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: 'app/index.html'
